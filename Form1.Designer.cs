cc9d08c82ce7269b2e18030825b04ef6c4834b78
b0boto <danila.rudackov@mail.ru>
2022-10-30 17:53:02 +0300

    Создал класс для шифрования XOR

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(392, 357);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(420, 145);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.PlaceholderText = "Текст который хотите зашифровать";
+            this.textBox1.Size = new System.Drawing.Size(222, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(418, 195);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PlaceholderText = "Ключ для шифрования";
+            this.textBox2.Size = new System.Drawing.Size(224, 23);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(301, 307);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.PlaceholderText = "Зашифрованный текст";
+            this.textBox3.Size = new System.Drawing.Size(220, 23);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(542, 307);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.PlaceholderText = "Расшифрованный текст";
+            this.textBox4.Size = new System.Drawing.Size(206, 23);
+            this.textBox4.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(596, 239);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(559, 357);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 40);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1155, 603);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button button1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Label label1;
+        private Button button2;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,8 @@
+using static System.Net.Mime.MediaTypeNames;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Text;
+
 namespace Decoder_pro
 {
     public partial class Form1 : Form
@@ -5,6 +10,59 @@ namespace Decoder_pro
         public Form1()
         {
             InitializeComponent();
+        }
+        public void button1_Click(object sender, EventArgs e)
+        {
+            var x = new XORCipher();
+            var message = textBox1.Text;
+            var pass = textBox2.Text;
+            var encryptedMessageByPass = x.Encrypt(message, pass);
+            textBox3.Text = encryptedMessageByPass;
+            textBox4.Text = x.Decrypt(encryptedMessageByPass, pass);
+        }
+
+        public class XORCipher
+        {
+            //генератор повторений пароля
+            private string GetRepeatKey(string s, int n)
+            {
+                var r = s;
+                while (r.Length < n)
+                {
+                    r += r;
+                }
+
+                return r.Substring(0, n);
+            }
+            //метод шифрования/дешифровки
+            private string Cipher(string text, string secretKey)
+            {
+                var currentKey = GetRepeatKey(secretKey, text.Length);
+                var res = string.Empty;
+                for (var i = 0; i < text.Length; i++)
+                {
+                    res += ((char)(text[i] ^ currentKey[i])).ToString();
+                }
+
+                return res;
+            }
+
+            //шифрование текста
+            public string Encrypt(string plainText, string password)
+                => Cipher(plainText, password);
+
+            //расшифровка текста
+            public string Decrypt(string encryptedText, string password)
+                => Cipher(encryptedText, password);
+        }
+
+        public void button2_Click(object sender, EventArgs e)
+        {
+            var x = new XORCipher();
+            var message = textBox1.Text;
+            var pass = textBox2.Text;
+            var encryptedMessageByPass = x.Encrypt(message, pass);
+            
         }
     }
 }
