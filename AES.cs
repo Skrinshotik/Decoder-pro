6ddfcaa97432ecc0a61a4ced8a99a548240fda43
slava <slava-kisel-2017@mail.ru>
2022-11-07 19:32:20 +0300

    Добавил файл класса AES.cs Это класс реализующий шифрование одноименным методом.

---
+++ AES.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace Decoder_pro
+{
+    internal class AES
+    {
+        /*Тест шифрования:
+         private static byte[] IV = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+         private void encode_button_Click(object sender, EventArgs e)
+         {
+         AES aes = new AES();
+         public_key_1_text.Text = aes.Encrypt("Бэтмен and Batman","1234567890123456",IV);
+         }*/
+        /*Тест дешифрования:
+         private static byte[] IV = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+         AES aes = new AES();
+         MessageBox.Show(aes.Decrypt("d8CagLwe4XX9kjgVLgO5NHSVnsezIX3/1Ftz+3ICQ50=", "1234567890123456", IV));
+         }*/
+        //private static byte[] IV = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+        public string Encrypt(string plainText, string Password, byte[] IV)//Зашифровать
+            {
+                byte[] Key = Encoding.UTF8.GetBytes(Password);
+
+                // Create a new AesManaged.    
+                AesManaged aes = new AesManaged();
+                aes.Key = Key;
+                aes.IV = IV;
+
+                MemoryStream memoryStream = new MemoryStream();
+                CryptoStream cryptoStream = new CryptoStream(memoryStream, aes.CreateEncryptor(), CryptoStreamMode.Write);
+
+                byte[] InputBytes = Encoding.UTF8.GetBytes(plainText);
+                cryptoStream.Write(InputBytes, 0, InputBytes.Length);
+                cryptoStream.FlushFinalBlock();
+
+                byte[] Encrypted = memoryStream.ToArray();
+                // Return encrypted data    
+                return Convert.ToBase64String(Encrypted);
+            }
+
+            public string Decrypt(string plaintext, string Password, byte[] IV)//Расшифровать
+            {
+                byte[] Key = Encoding.UTF8.GetBytes(Password);
+
+                // Create a new AesManaged.    
+                AesManaged aes = new AesManaged();
+                aes.Key = Key;
+                aes.IV = IV;
+
+                MemoryStream memoryStream = new MemoryStream();
+                CryptoStream cryptoStream = new CryptoStream(memoryStream, aes.CreateDecryptor(), CryptoStreamMode.Write);
+
+                byte[] InputBytes = Convert.FromBase64String(plaintext);
+                cryptoStream.Write(InputBytes, 0, InputBytes.Length);
+                cryptoStream.FlushFinalBlock();
+
+                byte[] Decrypted = memoryStream.ToArray();
+                // Return encrypted data    
+                return UTF8Encoding.UTF8.GetString(Decrypted, 0, Decrypted.Length);
+            }
+        }
+    }
