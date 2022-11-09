3534c992adcb5a1876e939bf18ff70e9e90e4644
b0boto <danila.rudackov@mail.ru>
2022-11-09 03:08:05 +0300

    добавил 2 формы для декодера

---
+++ FormVigenere.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Decoder_pro
+{
+    public partial class FormVigenere : Form
+    {
+        public FormVigenere()
+        {
+            
+            InitializeComponent();
+        }
+       
+        private void decrypt_button_Click(object sender, EventArgs e)
+        {
+            if (key_text.Text != "" && message_text.Text != "") // если лейблы с ключем и текстом не пусты
+            {
+                var cipher = new VigenereCipher(); 
+                int language = 1; 
+                if (radioButton1.Checked == true) language = 1; // выбор языка с помощью кружков
+                else language = 2;
+                switch (language)
+                {
+                    case 1:
+                        cipher = new VigenereCipher();
+                        encrypted_text.Text = cipher.Decrypt(message_text.Text.ToUpper(), key_text.Text.ToUpper());
+                        break;
+                    case 2:
+                        cipher = new VigenereCipher("АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ");
+                        encrypted_text.Text = cipher.Decrypt(message_text.Text.ToUpper(), key_text.Text.ToUpper());
+                        break;
+                }
+            }
+            else MessageBox.Show("Введите значение ключа или текста");
+
+        }
+
+        private void encrypt_button_Click(object sender, EventArgs e)
+        {
+            if (key_text.Text != "" && message_text.Text != "") // если лейблы с ключем и текстом не пусты
+            {
+                var cipher = new VigenereCipher();
+                int language = 1;
+                if (radioButton1.Checked == true) language = 1; // выбор языка с помощью кружков
+                else language = 2;
+                switch (language)
+                {
+                    case 1:
+                        cipher = new VigenereCipher();
+                        encrypted_text.Text = cipher.Encrypt(message_text.Text.ToUpper(), key_text.Text.ToUpper()); // шифруем текст и выводим
+                        break;
+                    case 2:
+                        cipher = new VigenereCipher("АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ");
+                        encrypted_text.Text = cipher.Encrypt(message_text.Text.ToUpper(), key_text.Text.ToUpper()); // расшифровываем текст и выводим
+                        break;         
+                }                      
+            }
+            else MessageBox.Show("Введите значение ключа или текста");
+        }
+
+        private void DecoderPro_label_Click(object sender, EventArgs e)
+        {
+            Form1 frm = new Form1(); // переход к другой форме
+            frm.Show();
+            Hide();
+        }
+    }
+}
